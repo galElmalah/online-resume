181fdf5d6944da87c4076aff421801aefb9dcfeb
Gal Elmalah <gale@wix.com>
2019-12-28 18:03:16 +0200

    routing issues

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { GithubProvider } from './components/GithubProvider/index';
 import { ThemeProvider } from './components/ThemeProvider/index';
 ReactDOM.render(
-  <BrowserRouter>    <GithubProvider>
-    <ThemeProvider>
-<App />
-</ThemeProvider></GithubProvider></BrowserRouter>, document.getElementById('root'));
+  <HashRouter basename={'/online-resume'}>
+    <GithubProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </GithubProvider>
+  </HashRouter>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
