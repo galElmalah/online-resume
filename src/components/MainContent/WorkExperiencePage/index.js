d7fed6e01fa633e456f865dd6364f750ca6f670d
Gal Elmalah <gale@wix.com>
2020-01-02 08:13:08 +0200

    added work experience page

--- src/components/MainContent/WorkExperiencePage/index.js
+++ src/components/MainContent/WorkExperiencePage/index.js
@@ -1,61 +1,26 @@
 import * as React from 'react';
 import * as style from './WorkExperiencePage.module.scss';
-
-const jobs = [{
-  from: '11.7.2019',
-  to: 'Now',
-  company: {
-    name: 'Wix.com',
-    website: ''
-  },
-  jobTitle: 'Student software developer',
-  description: 'i worked on a variei worked on a variety of front end and back end projecs \n lallallai worked on a variety of front end and back end projecs \n lallallai worked on a variety of front end and back end projecs \n lallallaty of front end and back end projecs \n lallallaai worked on a variety of front end and back end projecs \n lallallai worked on a variety of front end and back end projecs \n lallallai worked on a variety of front end and back end projecs \n lallalla'
-},
-{
-  from: '11.7.2019',
-  to: 'Now',
-  company: {
-    name: 'Wix.com',
-    website: ''
-  },
-  jobTitle: 'Student software developer',
-  description: 'i worked on a variety of front end and back end projecs \n lallallaa'
-},
-{
-  from: '11.7.2019',
-  to: 'Now',
-  company: {
-    name: 'Wix.com',
-    website: ''
-  },
-  jobTitle: 'Student software developer',
-  description: 'i worked on a variety of front end and back end projecs \n lallallaa'
-},
-{
-  from: '11.7.2019',
-  to: 'Now',
-  company: {
-    name: 'Wix.com',
-    website: ''
-  },
-  jobTitle: 'Student software developer',
-  description: 'i worked on a variety of front end and back end projecs \n lallallaa'
-}]
+import { ThemeContext } from '../../ThemeProvider/index';
+import classnames from 'classnames'
+import { jobs } from './jobs'
 
 export const WorkExperiencePage = () => {
+  const [isNightMode] = React.useContext(ThemeContext)
+
   return (
     <div className={style.WorkExperiencePageContainer}>
       <h1>Work Experience</h1>
       {jobs.map((job, i) => (
-        <section key={i}className={style.jobCard}>
+        <section key={i}className={classnames({ [style.jobCard]: true, [style.night]: isNightMode })}>
           <div className={style.description}>
             <p>{job.description}</p>
           </div>
           <div className={style.details}>
             <h3>{job.jobTitle}</h3>
             <p>{`${job.from} - ${job.to}`} </p>
-            <p>{`at - ${job.company.name}`} </p>
+            <p>{`@${job.company.name}`} </p>
           </div>
+          <img src={`./${job.logo || 'logo192.png'}`}/>
         </section>))}
     </div>
   );
---
+++ src/components/MainContent/WorkExperiencePage/jobs.js
@@ -0,0 +1,44 @@
+export const jobs = [
+  {
+    from: '1.1.2017',
+    to: '1.4.2018',
+    company: {
+      name: 'SCE',
+      website: ''
+    },
+    jobTitle: 'Math & Physics tutor',
+    description: 'Tutor for mathematics and physics in a pre-academic program of the SCE college and Perach organization.'
+  },
+  {
+    from: '1.1.2018',
+    to: '1.4.2018',
+    company: {
+      name: 'SCE',
+      website: ''
+    },
+    jobTitle: 'Research assistant',
+    description: 'Research Assistant at Sami Shamoon College Of Engineering, where i worked on a map based desktop app for the IDF'
+  },
+
+  {
+    from: '11.7.2019',
+    to: 'Now',
+    company: {
+      name: 'Wix.com',
+      website: ''
+    },
+    jobTitle: 'Student software developer',
+    description: 'worked on a variety of web apps including the wix developer and wix docs',
+    logo: 'wix-logo.png'
+  },
+  {
+    from: '11.7.2019',
+    to: 'Now',
+    company: {
+      name: 'Wix.com',
+      website: ''
+    },
+    jobTitle: 'Software developer',
+    description: 'Working mainly on the documentation platform @wix which is a general web based solution from RPC to REST and JS documentations, while considering performance in every step of the way',
+    logo: 'wix-logo.png'
+  }]
